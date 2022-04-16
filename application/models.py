a605f8cc4f579c27b78a52eaaf6f694b2d5b6200
karljay98 <karljace@msn.com>
2022-04-16 15:04:55 +0000

    fixed a few bugs

--- application/models.py
+++ application/models.py
@@ -6,11 +6,11 @@ class Library(db.model):
     id = db.column(db.Integer, primary_key=True) # library_id is the primary key and a integer value
     name = db.column(db.string(100), nullable=False) # The name of library must be filled
     description = db.column(db.string(200))
-    books = db.relationship(“Books”. Backreef=”library”)
+    books = db.relationship('Books'. Backref='library')
     
 class Book(db.model):
     book_id = db.column(db.integer, primary_kay=True)
     title = db.column(db.string(100), nullable=False)
     author = db.column(db.string(200))
     genre = db.column(db.string(75))
-    library_id = db.column(db.integer, db.foreignkey(“library_id”))
+    library_id = db.column(db.integer, db.foreignkey('library_id'))
