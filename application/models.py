e2109d341b60d74c80c66f9470e5f6aa1d19b703
karljay98 <karljace@msn.com>
2022-04-18 13:11:44 +0000

    small model changes

--- application/models.py
+++ application/models.py
@@ -4,14 +4,17 @@ from application import db
 
 class Library(db.Model):
     __tablename__ = 'library'
-    name = db.Column(db.String(100),priary_Key=True, nullable=False) # The name of library must be filled
+
+    id = db.Column(db.Integer, primary_Key=True)
+    name = db.Column(db.String(100), nullable=False) # The name of library must be filled
     description = db.Column(db.String(200), nullable=True)
     books = db.relationship('Book', backref='library')
     
 class Book(db.Model):
     __tablename__= 'book'
-    book_id = db.Column(db.Integer, primary_key=True)
+
+    book_id = db.Column(db.Integer, primary_Key=True)
     title = db.Column(db.String(100), nullable=False)
     author = db.Column(db.String(200), nullable = False)
     genre = db.Column(db.String(75), nullable = True)
-    library_name = db.Column(db.Integer, db.ForeignKey('library.name'), nullable=False)
+    library_id = db.Column(db.Integer, db.ForeignKey('library.id'), nullable=False)
